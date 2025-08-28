e3db91479f35f452586445113d6b87f8ef4a80e2
Dushyna <olgavnv777@gmail.com>
2025-08-28 16:38:57 +0200

    Create task02 change

--- src/main/java/task01/Class02.java
+++ src/main/java/task01/Class02.java
@@ -4,6 +4,6 @@ public class Class02 {
 
     public static void main(String[] args) {
 
-        System.out.println("Task01 Class02");
+        System.out.println("Task01 Class02 внесла изменения");
     }
 }
--- src/main/java/task01/Class03.java
+++ src/main/java/task01/Class03.java
@@ -4,6 +4,6 @@ public class Class03 {
 
     public static void main(String[] args) {
 
-        System.out.println("Task01 Clas03");
+        System.out.println("Task01 Clas03 внесла изменения");
     }
 }
