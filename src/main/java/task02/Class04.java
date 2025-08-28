bdb62858df279c8f63049403dacea0c08db97850
Dushyna <olgavnv777@gmail.com>
2025-08-28 16:28:48 +0200

    Create task02

---
+++ src/main/java/task02/Class04.java
@@ -0,0 +1,9 @@
+package task02;
+
+public class Class04 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Task02 Class04");
+    }
+}
---
+++ src/main/java/task02/Class05.java
@@ -0,0 +1,9 @@
+package task02;
+
+public class Class05 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Task02 Class05");
+    }
+}
